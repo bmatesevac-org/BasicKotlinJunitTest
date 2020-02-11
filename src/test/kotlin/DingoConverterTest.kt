0bfa719d782ac14d7c0092e2d91055ba6c1495b6
Bob Matesevac <bob.matesevac@willowtreeapps.com>
2020-02-11 10:49:03 -0500

    First cut at bare bones.

---
+++ src/test/kotlin/DingoConverterTest.kt
@@ -0,0 +1,21 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class DingoTestCasss
+{
+    @Test
+    @DisplayName("Test 001")
+    fun Test001()
+    {
+        println("Running Test001!")
+    }
+
+    @Test
+    @DisplayName("Test 002")
+    fun Test002()
+    {
+        println("Running Test002!")
+    }
+}
